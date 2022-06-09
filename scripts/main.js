d47c3ed2ef15ef045b13f9be3e8a54e02cf88117
djordje.sinikovic <djordjesinikovic@gmail.com>
2022-06-10 01:47:26 +0200

    course page added

--- scripts/main.js
+++ scripts/main.js
@@ -49,6 +49,14 @@ $(document).ready(function () {
       }
     });
   });
+
+  $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+  });
 });
 
 var splide = new Splide(".splide-testimonials", {
@@ -78,3 +86,55 @@ var splide = new Splide(".splide-testimonials", {
   },
 });
 splide.mount();
+
+function handleTickInit(tick) {
+  // uncomment to set labels to different language
+  /*
+  var locale = {
+      YEAR_PLURAL: 'Jaren',
+      YEAR_SINGULAR: 'Jaar',
+      MONTH_PLURAL: 'Maanden',
+      MONTH_SINGULAR: 'Maand',
+      WEEK_PLURAL: 'Weken',
+      WEEK_SINGULAR: 'Week',
+      DAY_PLURAL: 'Dagen',
+      DAY_SINGULAR: 'Dag',
+      HOUR_PLURAL: 'Uren',
+      HOUR_SINGULAR: 'Uur',
+      MINUTE_PLURAL: 'Minuten',
+      MINUTE_SINGULAR: 'Minuut',
+      SECOND_PLURAL: 'Seconden',
+      SECOND_SINGULAR: 'Seconde',
+      MILLISECOND_PLURAL: 'Milliseconden',
+      MILLISECOND_SINGULAR: 'Milliseconde'
+  };
+
+  for (var key in locale) {
+      if (!locale.hasOwnProperty(key)) { continue; }
+      tick.setConstant(key, locale[key]);
+  }
+  */
+
+  // format of due date is ISO8601
+  // https://en.wikipedia.org/wiki/ISO_8601
+
+  // '2018-01-31T12:00:00'        to count down to the 31st of January 2018 at 12 o'clock
+  // '2019'                       to count down to 2019
+  // '2018-01-15T10:00:00+01:00'  to count down to the 15th of January 2018 at 10 o'clock in timezone GMT+1
+
+  // create the countdown counter
+  var counter = Tick.count.down("2022-09-01T00:00:00+01:00");
+
+  counter.onupdate = function (value) {
+    tick.value = value;
+  };
+
+  counter.onended = function () {
+    // redirect, uncomment the next line
+    // window.location = 'my-location.html'
+    // hide counter, uncomment the next line
+    // tick.root.style.display = 'none';
+    // show message, uncomment the next line
+    // document.querySelector('.tick-onended-message').style.display = '';
+  };
+}
